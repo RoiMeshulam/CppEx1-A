aa26fcefb8a2d01b78e93a43256e00f4439cc5d5
RoiMeshulam <rohimesh21@gmail.com>
2022-03-08 19:02:42 +0200

    first commit

---
+++ mat.cpp
@@ -0,0 +1,10 @@
+#include "mat.hpp"
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+string ariel::mat(int a, int b, char c1, char c2) {
+
+    return "check";
+}
